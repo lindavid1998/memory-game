c1a99709af25c384df9d64a9651048c56564588c
David Lin <lindavid1998@gmail.com>
2023-04-08 14:26:52 -0700

    Create basic structure and state of App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React, { useState } from 'react';
 import './App.css';
+import messiImg from './images/messi.jpeg';
+import ronaldoImg from './images/ronaldo.jpeg';
+import neymarImg from './images/neymar.jpg';
+import mitomaImg from './images/mitoma.jpeg';
 
 function App() {
-	return <div></div>;
+	const [highScore, setHighScore] = useState(0);
+	const [currentScore, setCurrentScore] = useState(0);
+	const [visitedPlayers, setVisitedPlayers] = useState([]);
+
+	const players = [
+		{ name: 'Lionel Messi', src: messiImg },
+		{ name: 'Cristiano Ronaldo', src: ronaldoImg },
+		{ name: 'Karou Mitoma', src: neymarImg },
+		{ name: 'Neymar', src: mitomaImg },
+	];
+
+	return (
+		<div>
+			<div className='scores'>
+				<div className='high-score'>High score: {highScore}</div>
+				<div className='current-score'>Current score: {currentScore}</div>
+			</div>
+
+			<div>
+				{players.map((player, i) => {
+					return (
+						<div key={i}>
+							<img src={player.src} alt='Player'></img>
+							<p>{player.name}</p>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
