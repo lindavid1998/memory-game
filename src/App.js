591a014d00835237bbedfb8ab891c1c82a66a130
David Lin <lindavid1998@gmail.com>
2023-04-09 15:08:28 -0700

    Add event handlers to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import messiImg from './images/messi.jpeg';
 import ronaldoImg from './images/ronaldo.jpeg';
@@ -10,13 +10,51 @@ export default function App() {
 	const [highScore, setHighScore] = useState(0);
 	const [currentScore, setCurrentScore] = useState(0);
 	const [visitedPlayers, setVisitedPlayers] = useState([]);
-
-	const players = [
+	const [players, setPlayers] = useState([
 		{ name: 'Lionel Messi', src: messiImg },
 		{ name: 'Cristiano Ronaldo', src: ronaldoImg },
 		{ name: 'Kaoru Mitoma', src: mitomaImg },
 		{ name: 'Neymar', src: neymarImg },
-	];
+	]);
+
+	// - whenever a player is clicked
+	const handleClick = (e) => {
+		const playerDiv = e.currentTarget;
+		const name = playerDiv.querySelector('p').textContent;
+
+		if (visitedPlayers.includes(name)) {
+			setHighScore(Math.max(currentScore, highScore));
+			setCurrentScore(0);
+			setVisitedPlayers([]);
+		} else {
+			setVisitedPlayers(visitedPlayers.concat([name]));
+			setCurrentScore(currentScore + 1);
+    }
+  };
+
+	useEffect(() => {
+		const shuffleArray = (array) => {
+			for (let i = array.length - 1; i > 0; i--) {
+				const randomIndex = getRandomIntInclusive(0, i);
+				swapElements(array, i, randomIndex);
+			}
+			return array;
+		};
+
+		const getRandomIntInclusive = (min, max) => {
+			// Generates a random integer between min and max (inclusive).
+			const minCeil = Math.ceil(min);
+			const maxFloor = Math.floor(max);
+			return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
+		};
+
+		const swapElements = (array, index1, index2) => {
+			// Swaps the elements at index1 and index2 in array.
+			[array[index1], array[index2]] = [array[index2], array[index1]];
+		};
+
+		setPlayers(shuffleArray(players));
+	}, [currentScore, players]);
 
 	return (
 		<div className='App'>
@@ -26,10 +64,15 @@ export default function App() {
 			</div>
 
 			<div className='players'>
-        {players.map((player, i) => {
-          return (
-            <Player imgSrc={player.src} name={player.name} key={i} />
-          );
+				{players.map((player, i) => {
+					return (
+						<Player
+							imgSrc={player.src}
+							name={player.name}
+							onClick={handleClick}
+							key={i}
+						/>
+					);
 				})}
 			</div>
 		</div>
--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function Player(props) {
-  const { imgSrc, name } = props;
+  const { imgSrc, name, onClick } = props;
   return (
-    <div className='Player'>
+    <div className='Player' onClick={onClick}>
       <img src={imgSrc} alt={'Image of ' + name}></img>
       <p>{name}</p>
     </div>
